044499a3b2d02efded5c7844674d6621d3b17678
xqs <1219651789@qq.com>
2021-11-12 11:16:05 +0800

    热线趋势选择

---
+++ src/components/echart/top/topLeftChart/chart.vue
@@ -0,0 +1,239 @@
+<template>
+  <div>
+    <!-- 热线话务历史趋势 -->
+    <Echart
+      :options="options"
+      id="bottomLeftChart"
+      height="260px"
+      width="100%"
+    ></Echart>
+  </div>
+</template>
+
+<script>
+import Echart from '@/common/echart'
+export default {
+  data () {
+    return {
+      options: {},
+    };
+  },
+  components: {
+    Echart,
+  },
+  props: {
+    cdata: {
+      type: Object,
+      default: () => ({})
+    },
+  },
+  watch: {
+    cdata: {
+      handler (newData) {
+        this.options = {
+          color: ['#C33DC7'],
+          title: {
+            text: '话务总量',
+            top:0,
+            left:0,
+            padding:8,
+            backgroundColor:'#394280',
+            textStyle: {
+            fontSize: 17,
+            align: 'center',
+            fontFamily:'PingFangSC-Light',
+            fontWeight:'lighter',
+            width: 84,
+            height:34,
+            color:'#F4F5F8'
+            },
+          },
+          tooltip: {
+            trigger: 'axis',
+            axisPointer: {
+              type: 'cross',
+              label: {
+                backgroundColor: '#6a7985'
+              }
+            }
+          },
+          // legend: {
+          //   left :'center',
+          //   data: ['BICC', 'ISBC', 'RTC'],
+          //   icon: "circle",
+          //   padding:8,
+          //   itemWidth: 11,
+          //   textStyle: {
+          //   fontSize: 17
+          //   },
+          // },
+          grid: {
+            top:'50',
+            left: '0',
+            right: '10',
+            bottom: '0',
+            containLabel: true
+          },
+          xAxis: [
+            {
+              type: 'category',
+              boundaryGap: false,
+              data: newData.category,
+            }
+          ],
+          yAxis: [
+            {
+              type: 'value'
+            }
+          ],
+          series: [
+            // {
+            //   name: 'BICC',
+            //   type: 'line',
+            //   // stack: 'Total',        //数据堆叠
+            //   smooth: true,          //是否平滑曲线显示
+            //   lineStyle: {
+            //     width: 0.6
+            //   },
+            //   showSymbol: false,
+            //   z:1,
+            //   areaStyle: {
+            //     opacity: 0.8,
+            //     color: {
+            //           type: 'linear',
+            //           x: 0,
+            //           y: 0,
+            //           x2: 0,
+            //           y2: 1,
+            //           colorStops: [{
+            //               offset: 0, color: 'rgba(13, 144, 234, 0.5)', // 0% 处的颜色
+            //           }, {
+            //               offset: 1, color: 'rgba(13, 144, 234, 0)' // 100% 处的颜色
+            //           }],
+            //           global: false // 缺省为 false
+            //         }
+            //    },
+            //   emphasis: {
+            //     focus: 'series'
+            //   },
+            //   data: [40, 32, 91, 64, 90, 40, 100]
+            // },
+            // {
+            //   name: 'BICC',
+            //   type: 'line',
+            //   // stack: 'Total',
+            //   smooth: true,
+            //   lineStyle: {
+            //     type: "dashed",
+            //     width: 0.6
+            //   },
+            //   showSymbol: false,
+            //   z:1,
+            //   emphasis: {
+            //     focus: 'series'
+            //   },
+            //   data: [90, 82, 51, 14, 40, 90, 50,90, 82, 51, 14, 40, 90, 50,90, 82, 51, 14, 40, 90, 50, 40, 90, 50]
+            // },
+            // {
+            //   name: 'ISBC',
+            //   type: 'line',
+            //   // stack: 'Total',
+            //   smooth: true,
+            //   lineStyle: {
+            //     width: 0.6
+            //   },
+            //   showSymbol: false,
+            //   z:2,
+            //   areaStyle: {
+            //     opacity: 0.8,
+            //     color: {
+            //           type: 'linear',
+            //           x: 0,
+            //           y: 0,
+            //           x2: 0,
+            //           y2: 1,
+            //           colorStops: [{
+            //               offset: 0, color: 'rgba(199, 126, 62, 0.5)', // 0% 处的颜色
+            //           }, {
+            //               offset: 1, color: 'rgba(199, 126, 62, 0)' // 100% 处的颜色
+            //           }],
+            //           global: false // 缺省为 false
+            //         }
+            //   },
+            //   emphasis: {
+            //     focus: 'series'
+            //   },
+            //   data: [120, 182, 111, 134, 200, 140, 110]
+            // },
+            // {
+            //   name: 'ISBC',
+            //   type: 'line',
+            //   // stack: 'Total',
+            //   smooth: true,
+            //   lineStyle: {
+            //     type: "dashed",
+            //     width: 0.6
+            //   },
+            //   showSymbol: false,
+            //   z:2,
+            //   emphasis: {
+            //     focus: 'series'
+            //   },
+            //   data: [190, 192, 191, 194, 220, 190, 190,190, 192, 191, 194, 220, 190, 190,190, 192, 191, 194, 220, 190, 190, 220, 190, 190]
+            // },
+            {
+              name: '话务总量',
+              type: 'line',
+              // stack: 'Total',
+              smooth: true,
+              lineStyle: {
+                width: 1
+              },
+              showSymbol: false,
+              z:3,
+              areaStyle: {
+                opacity: 0.8,
+                color: {
+                      type: 'linear',
+                      x: 0,
+                      y: 0,
+                      x2: 0,
+                      y2: 1,
+                      colorStops: [{
+                          offset: 0, color: 'rgba(195, 61, 199, 0.5)', // 0% 处的颜色
+                      }, {
+                          offset: 1, color: 'rgba(195, 61, 199, 0)' // 100% 处的颜色
+                      }],
+                      global: false // 缺省为 false
+                    }
+              },
+              emphasis: {
+                focus: 'series'
+              },
+              data: [220, 232, 201, 334, 290, 230, 320]
+            },
+            {
+              name: '话务总量',
+              type: 'line',
+              // stack: 'Total',
+              smooth: true,
+              lineStyle: {
+                type: "dashed",
+                width: 1
+              },
+              showSymbol: false,
+              z:3,
+              emphasis: {
+                focus: 'series'
+              },
+              data: [300, 402, 301, 404, 400, 300, 290,310, 402, 301, 404, 400, 300, 290,310, 402, 301, 404, 400, 300, 290, 400, 300, 290]
+            }
+          ]
+        }
+      },
+      immediate: true,
+      deep: true
+    },
+  },
+}
+</script>
---
+++ src/views/mainSelect.vue
@@ -0,0 +1,132 @@
+<template>
+  <div id="mainSelect">
+    <el-select :popper-append-to-body="false" popper-class="now-select-style" v-model="operator" placeholder="10086" style="width:88px">
+      <el-option
+        v-for="item in options1"
+        :key="item.value"
+        :label="item.label"
+        :value="item.value">
+      </el-option>
+    </el-select>
+    <el-select :popper-append-to-body="false" popper-class="now-select-style" v-model="panorama" placeholder="全景图" style="width:120px;margin-left:23px">
+      <el-option
+        v-for="item in options2"
+        :key="item.value"
+        :label="item.label"
+        :value="item.value">
+      </el-option>
+    </el-select>
+    <el-select :popper-append-to-body="false" popper-class="now-select-style" v-model="cities" placeholder="地市" style="width:80px;margin-left:23px">
+      <el-option
+        v-for="item in options3"
+        :key="item.value"
+        :label="item.label"
+        :value="item.value">
+      </el-option>
+    </el-select>
+    <el-select :popper-append-to-body="false" popper-class="now-select-style" v-model="starLevel" placeholder="星级" style="width:80px;margin-left:23px">
+      <el-option
+        v-for="item in options4"
+        :key="item.value"
+        :label="item.label"
+        :value="item.value">
+      </el-option>
+    </el-select>
+  </div>
+</template>
+
+<script>
+export default {
+   data() {
+      return {
+        options1: [{
+          value: '10086',
+          label: '10086'
+        }],
+        operator: '',
+        options2: [{
+          value: 'q1',
+          label: '全景图'
+        },{
+          value: 'q2',
+          label: '地市星级'
+        }],
+        panorama: '',
+        options3: [{
+          value: 'd1',
+          label: '昆明'
+        },{
+          value: 'd2',
+          label: '曲靖'
+        },{
+          value: 'd3',
+          label: '丽江'
+        },{
+          value: 'd4',
+          label: '玉溪'
+        }],
+        cities: '',
+        options4: [{
+          value: 'x1',
+          label: '星级'
+        },{
+          value: 'x2',
+          label: '星级'
+        }],
+        starLevel: '',
+      }
+    }
+};
+</script>
+
+<style lang="scss" class>
+$box-height: 100%;
+$box-width: 100%;
+#mainSelect{
+  padding: 29px 23px;
+  height: $box-height;
+  width: $box-width;
+  position: relative;
+   .el-select >div>input{
+      background-color:transparent;
+      border: 1px solid rgba(22, 56, 141, 0.8);
+      font-size: 15px;
+      color:#FFFFFF;
+   }
+   .el-select >div>input:focus{
+      border: 1px solid #16388D;
+   }
+   .el-select .now-select-style{
+      background-color:#16244D;
+      border: 1px solid #16388D;
+      padding:0 0;
+      font-size: 15px;
+      .el-select-dropdown__item{
+        color:#FFFFFF;
+      }
+      .el-select-dropdown__item.hover, .el-select-dropdown__item:hover{
+        background-color:#16388D;
+        color:#FFFFFF;
+        // font-weight: bold;
+        // font-size:110%;
+      }
+       .popper__arrow{
+          border-bottom-color:#16388D;
+          left:70% !important;
+        }
+       .popper__arrow::after{
+        border-bottom-color:#16244D;
+      }
+   }
+   .el-icon-arrow-up:before {
+     color:#41B5F0;
+     content: "\e790";
+   }
+}
+  // .now-select-style,input{
+  //   background-color:transparent;
+  //   border: 1px solid #16388D;
+// .el-select-dropdown{
+//   background-color:transparent;
+// }
+</style>
