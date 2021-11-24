ae29f1d143a61d5d02a60139e28b713dad7d0480
hxwly <1812538763@qq.com>
2021-11-24 16:34:21 +0800

    整体坐标修改

--- src/views/largeScreen/d3Data.js
+++ src/views/largeScreen/d3Data.js
@@ -8,27 +8,30 @@ const dataset = {
 		"interfaceVal": 200, //接口异常量
 		"isFold": true, //是否展开节点 默认true 展开
 		"type": 1, //类型
-		x: 720,
-		y: -35,
+		x: 800,
+		y: 0,
 		id: 1,
 		"children": [
 			{
 				x: 580,
-				y: 160,
-				x2: 580,
-				y2: 190,
+				y: 60,
+				x2: 590,
+				y2: 90,
 				value: 1500,
+				//文本坐标
+				textX:570,
+				textY:130,
 				//箭头坐标
 				bottomX: 600,
-				bottomY: 240,
+				bottomY: 140,
 				//数值坐标
 				valueX: 585,
-				valueY: 150,
+				valueY: 50,
 				width: 50,
 				height: 50,
 				//标记点
-				pointX: 700,
-				pointY: 185,
+				pointX: 730,
+				pointY: 80,
 				pointUrl: require("../../assets/images/light1.png"),
 				"name": "互联网话务",
 				"type": 1, //类型
@@ -41,20 +44,23 @@ const dataset = {
 			},
 			{
 				x: 1210,
-				y: 160,
+				y: 60,
 				x2: 1220,
-				y2: 190,
+				y2: 90,
 				id: 4,
 				value: 900,
+				//文本坐标
+				textX:1205,
+				textY:130,
 				bottomX: 1230,
-				bottomY: 240,
+				bottomY: 140,
 				//数值坐标
-				valueX: 1215,
-				valueY: 150,
+				valueX: 1220,
+				valueY: 50,
 				width: 50,
 				height: 50,
-				pointX: 1120,
-				pointY: 183,
+				pointX: 1100,
+				pointY: 80,
 				pointUrl: require("../../assets/images/light5.png"),
 				"name": "跨网支撑",
 				"type": 2, //类型
@@ -64,25 +70,28 @@ const dataset = {
 				"interfaceVal": 200 //接口异常量
 			},
 			{
-				x: 640,
-				y: 270,
+				x: 680,
+				y: 160,
 				x2: 700,
-				y2: 310,
+				y2: 215,
 				id: 5,
 				value: 2000,
-				bottomX: 660,
-				bottomY: 350,
+				bottomX: 700,
+				bottomY: 250,
+				//文本坐标
+				textX:675,
+				textY:240,
 				//数值坐标
-				valueX: 650,
-				valueY: 260,
+				valueX: 690,
+				valueY: 150,
 				width: 60,
 				height: 60,
-				pointX: 760,
-				pointY: 265,
+				pointX: 790,
+				pointY: 148,
 				pointUrl: require("../../assets/images/light2.png"),
 				//贴片
-				patchX: 827,
-				patchY: 226,
+				patchX: 845,
+				patchY: 115,
 				patchUrl: require("../../assets/images/patch1.png"),
 				"name": "语音话务",
 				"type": 3, //类型
@@ -100,19 +109,18 @@ const dataset = {
 						"img": "",
 						"x": 690,
 						"y": 550,
-						"imgX": 620, //大图标位移
-						"imgY": 420,
-						"spreadX": 668,//加号减号坐标
-						"spreadY": 440,
-						"nameX": 620,//节点名称位移
-						"nameY": 520,
-						"totalNodeX": 620,//节点总量位移
-						"totalNodeY": 540,
-						"trendImgX": 716,//趋势图表位移
-						"trendImgY": 526,
-						bottomX: 660,
-						bottomY: 546,
-						"pathD": "M668 360 L 668 450",
+						"imgX": 655, //大图标位移
+						"imgY": 300,
+						"spreadX": 705,//加号减号坐标
+						"spreadY": 320,
+						"nameX": 655,//节点名称位移
+						"nameY": 395,
+						"totalNodeX": 640,//节点总量位移
+						"totalNodeY": 415,
+						"trendImgX": 735,//趋势图表位移
+						"trendImgY": 400,
+						bottomX: 690,
+						bottomY: 430,
 						"totalVal": 700, //节点总量
 						"abnormalVal": 300, //异常挂断量
 						"interfaceVal": 200, //接口异常量
@@ -128,19 +136,19 @@ const dataset = {
 								"y": 620,
 								bottomX: 446,
 								bottomY: 704,
-								"imgX": 400, //大图标位移
-								"imgY": 580,
-								"spreadX": 450,//加号减号坐标
-								"spreadY": 600,
-								"nameX": 420,//节点名称位移
-								"nameY": 680,
-								"totalNodeX": 420,//节点总量位移
-								"totalNodeY": 700,
+								"imgX": 405, //大图标位移
+								"imgY": 460,
+								"spreadX": 455,//加号减号坐标
+								"spreadY": 480,
+								"nameX": 430,//节点名称位移
+								"nameY": 555,
+								"totalNodeX": 410,//节点总量位移
+								"totalNodeY": 575,
 								"nodeX": 600,//节点量位移
 								"nodeY": 500,
-								"trendImgX": 510,//趋势图表位移
-								"trendImgY": 690,
-								"pathD": "M668 554 C 650 596,448 560,448 600",//曲线位置
+								"trendImgX": 500,//趋势图表位移
+								"trendImgY": 560,
+								"pathD": "M695 440 C 710 470 490 430,450 480",//曲线位置
 								"totalVal": 700, //节点总量
 								"value": 100, //节点量
 								"abnormalVal": 300, //异常挂断量
@@ -471,19 +479,19 @@ const dataset = {
 								"id": 1,
 								"x": 700,
 								"y": 540,
-								"imgX": 620, //大图标位移
-								"imgY": 580,
-								"spreadX": 668,//加号减号坐标
-								"spreadY": 600,
-								"nameX": 620,//节点名称位移
-								"nameY": 680,
-								"totalNodeX": 620,//节点总量位移
-								"totalNodeY": 700,
+								"imgX": 650, //大图标位移
+								"imgY": 460,
+								"spreadX": 697,//加号减号坐标
+								"spreadY": 480,
+								"nameX": 675,//节点名称位移
+								"nameY": 555,
+								"totalNodeX": 650,//节点总量位移
+								"totalNodeY": 575,
 								"nodeX": 640,//节点量位移
 								"nodeY": 460,
-								"trendImgX": 710,//趋势图表位移
-								"trendImgY": 690,
-								"pathD": "M668 554 V 620 610",//曲线位置
+								"trendImgX": 740,//趋势图表位移
+								"trendImgY": 560,
+								"pathD": "M697 440 L 697 480",//曲线位置
 								"totalVal": 700, //节点总量
 								"value": 100, //节点量
 								"abnormalVal": 300, //异常挂断量
@@ -498,19 +506,19 @@ const dataset = {
 								"img": "",
 								"x": 840,
 								"y": 580,
-								"imgX": 840, //大图标位移
-								"imgY": 580,
-								"spreadX": 885,//加号减号坐标
-								"spreadY": 600,
-								"nameX": 840,//节点名称位移
-								"nameY": 680,
-								"totalNodeX": 840,//节点总量位移
-								"totalNodeY": 700,
-								"nodeX": 840,//节点量位移
+								"imgX": 860, //大图标位移
+								"imgY": 460,
+								"spreadX": 905,//加号减号坐标
+								"spreadY": 480,
+								"nameX": 875,//节点名称位移
+								"nameY": 555,
+								"totalNodeX": 860,//节点总量位移
+								"totalNodeY": 575,
+								"nodeX": 860,//节点量位移
 								"nodeY": 460,
-								"trendImgX": 930,//趋势图表位移
-								"trendImgY": 690,
-								"pathD": "M668 554 C 780 606,810 560,890 600",//曲线位置
+								"trendImgX": 950,//趋势图表位移
+								"trendImgY": 560,
+								"pathD": "M695 440 C 730 470,900 450,905 480",//曲线位置
 								"totalVal": 700, //节点总量
 								"abnormalVal": 300, //异常挂断量
 								"interfaceVal": 200 //接口异常量
@@ -520,25 +528,28 @@ const dataset = {
 				]
 			},
 			{
-				x: 880,
-				y: 350,
+				x: 890,
+				y: 190,
 				x2: 920,
-				y2: 360,
+				y2: 200,
 				id: 2,
 				width: 70,
 				height: 70,
 				value: 1200,
-				bottomX: 910,
-				bottomY: 430,
+				//文本坐标
+				textX:895,
+				textY:280,
+				bottomX: 920,
+				bottomY: 290,
 				//数值坐标
 				valueX: 930,
-				valueY: 345,
+				valueY: 180,
 				pointX: 910,
-				pointY: 300,
+				pointY: 160,
 				pointUrl: require("../../assets/images/light3.png"),
 				//贴片
 				patchX: 910,
-				patchY: 238,
+				patchY: 135,
 				patchUrl: require("../../assets/images/patch2.png"),
 				"name": "溢出流程",
 				"type": 4, //类型
@@ -548,25 +559,28 @@ const dataset = {
 				"interfaceVal": 200 //接口异常量
 			},
 			{
-				x: 1140,
-				y: 270,
-				x2: 1160,
-				y2: 320,
+				x: 1120,
+				y: 160,
+				x2: 1150,
+				y2: 215,
 				id: 6,
 				value: 1000,
-				bottomX: 1165,
-				bottomY: 350,
+				bottomX: 1145,
+				bottomY: 250,
 				width: 60,
 				height: 60,
+				//文本坐标
+				textX:1120,
+				textY:240,
 				//数值坐标
-				valueX: 1150,
-				valueY: 260,
-				pointX: 1060,
-				pointY: 265,
+				valueX: 1130,
+				valueY: 150,
+				pointX: 1040,
+				pointY: 150,
 				pointUrl: require("../../assets/images/light4.png"),
 				//贴片
-				patchX: 985,
-				patchY: 220,
+				patchX: 975,
+				patchY: 116,
 				patchUrl: require("../../assets/images/patch3.png"),
 				"name": "视频话务",
 				"type": 4, //类型
--- src/views/largeScreen/progress.vue
+++ src/views/largeScreen/progress.vue
@@ -1,306 +1,289 @@
 <template>
   <g class="progress">
     <g v-for="(item, index) in d3Data.dataset.nodes.children" :key="index">
+      <line v-if="item.children !== undefined" :x1="item.bottomX + 8" :y1="item.bottomY + 5" :x2="item.children[0].imgX + 50" :y2="item.children[0].imgY + 25" style="stroke:#fff;stroke-width:1"></line>
       <g class="nodes" v-for="(item1, index1) in item.children" :key="index1">
-        <!-- 联线 -->
-        <g>
-          <circle r="5" cx="500" cy="400" fill="#fff"></circle>
-          <path :d="item1.pathD" stroke="#a09eff" fill="none" />
-        </g>
         <!-- 第一级 -->
         <g class="node" width="200" height="200">
-          <circle
-            :cx="item1.spreadX"
-            :cy="item1.spreadY"
-            r="6"
-            stroke="#fff"
-          ></circle>
-          <text
-            dy="4"
-            dx="-4"
-            :x="item1.spreadX"
-            :y="item1.spreadY"
-            fill="#fff"
-            font-size="13"
-            v-if="item1.isFold"
-            >+</text
-          >
-          <text
-            dy="3"
-            dx="-2"
-            :x="item1.spreadX"
-            :y="item1.spreadY"
-            font-size="13"
-            fill="#fff"
-            v-else
-            >-</text
-          >
+          <circle :cx="item1.spreadX" :cy="item1.spreadY" r="6" stroke="#fff"></circle>
+          <text dy="4" dx="-4" :x="item1.spreadX" :y="item1.spreadY" fill="#fff" font-size="13" v-if="item1.isFold">+</text>
+          <text dy="3" dx="-2" :x="item1.spreadX" :y="item1.spreadY" font-size="13" fill="#fff" v-else>-</text>
           <image
-            opacity="1"
-            stroke-width="1"
-            stroke-opacity="1"
-            fill-opacity="1"
-            width="100"
-            height="100"
-            :href="frontImg"
-            :x="item1.imgX"
-            :y="item1.imgY"
-          ></image>
-          <text
-            class="pro-text"
-            fill="#fff"
-            :x="item1.nameX"
-            :y="item1.nameY"
-            dx="20"
-            >{{ item1.name }}</text
-          >
-          <g>
-            <text
-              class="pro-num"
-              dx="20"
-              :x="item1.totalNodeX"
-              :y="item1.totalNodeY"
-              fill="#fff"
-              font="12"
-              >节点量 {{ item1.totalVal }}</text
-            >
-            <image
-              opacity="1"
-              stroke-width="1"
-              stroke-opacity="1"
-              fill-opacity="1"
-              :x="item1.trendImgX"
-              :y="item1.trendImgY"
-              width="18"
-              height="18"
-              :href="trendImg"
-            ></image>
-          </g>
-          <image
-            opacity="1"
-            stroke-width="1"
-            stroke-opacity="1"
-            fill-opacity="1"
-            :x="item1.bottomX"
-            :y="item1.bottomY"
-            width="15"
-            height="15"
-            :href="arrowUrl"
-          ></image>
-        </g>
-        <!-- 第二级 -->
-        <g
-          class="node"
-          width="200"
-          height="200"
-          v-for="(item2, index2) in item1.children"
-          :key="index2"
-        >
-          <!-- 联线 -->
-          <g>
-            <path :d="item2.pathD" stroke="#a09eff" fill="none" />
-          </g>
-          <circle
-            :cx="item2.spreadX"
-            :cy="item2.spreadY"
-            r="6"
-            stroke="#fff"
-          ></circle>
-          <text
-            dy="4"
-            dx="-4"
-            :x="item2.spreadX"
-            :y="item2.spreadY"
-            fill="#fff"
-            font-size="13"
-            v-if="item2.isFold"
-            >+</text
-          >
-          <text
-            dy="3"
-            dx="-2"
-            :x="item2.spreadX"
-            :y="item2.spreadY"
-            font-size="13"
-            fill="#fff"
-            v-else
-            >-</text
-          >
-          <image
-            opacity="1"
-            stroke-width="1"
-            stroke-opacity="1"
-            fill-opacity="1"
-            width="100"
-            height="100"
-            :x="item2.imgX"
-            :y="item2.imgY"
-            :href="frontImg"
-          ></image>
-          <g>
-            <text
-              class="pro-text"
-              :x="item2.nameX"
-              :y="item2.nameY"
-              fill="#fff"
-              >{{ item2.name }}</text
-            >
-            <image
-              opacity="1"
-              stroke-width="1"
-              stroke-opacity="1"
-              fill-opacity="1"
-              width="16"
-              height="16"
-              :x="item2.imgX"
-              :y="item2.imgY"
-              :href="InterfaceImg"
-            ></image>
-          </g>
-          <g>
-            <text
-              class="pro-num"
-              :x="item2.totalNodeX"
-              :y="item2.totalNodeY"
-              fill="#fff"
-              font="12"
-              >节点总量 {{ item2.totalVal }}</text
-            >
-            <image
-              opacity="1"
-              stroke-width="1"
-              stroke-opacity="1"
-              fill-opacity="1"
-              :x="item2.trendImgX"
-              :y="item2.trendImgY"
-              width="18"
-              height="18"
-              :href="trendImg"
-            ></image>
-          </g>
-          <image
-            opacity="1"
-            stroke-width="1"
-            stroke-opacity="1"
-            fill-opacity="1"
-            :x="item2.bottomX"
-            :y="item2.bottomY"
-            width="15"
-            height="15"
-            :href="arrowUrl"
-          ></image>
-          <!-- 第三级 -->
-          <g
-            class="node"
-            width="200"
-            height="200"
-            v-for="(item3, index3) in item2.children"
-            :key="index3"
-          >
-            <!-- 联线 -->
-            <g>
-              <path :d="item3.pathD" stroke="red" fill="none" />
-            </g>
-            <image
               opacity="1"
               stroke-width="1"
               stroke-opacity="1"
               fill-opacity="1"
               width="100"
               height="100"
-              :x="item3.imgX"
-              :y="item3.imgY"
               :href="frontImg"
-            ></image>
-            <g>
-              <text
-                class="pro-text"
-                :x="item3.nameX"
-                :y="item3.nameY"
-                fill="#fff"
-                >{{ item3.name }}</text
-              >
-              <image
-                opacity="1"
-                stroke-width="1"
-                stroke-opacity="1"
-                fill-opacity="1"
-                width="16"
-                height="16"
-                :x="item3.InterfaceImgX"
-                :y="item3.InterfaceImgY"
-                :href="InterfaceImg"
-              ></image>
-            </g>
-            <g>
-              <text
+              :x="item1.imgX"
+              :y="item1.imgY"
+          ></image>
+          <text
+              class="pro-text"
+              fill="#fff"
+              :x="item1.nameX"
+              :y="item1.nameY"
+              dx="20"
+          >{{ item1.name }}
+          </text
+          >
+          <g>
+            <text
                 class="pro-num"
-                :x="item3.totalNodeX"
-                :y="item3.totalNodeY"
+                dx="20"
+                :x="item1.totalNodeX"
+                :y="item1.totalNodeY"
                 fill="#fff"
                 font="12"
-                >节点总量 {{ item3.totalVal }}</text
-              >
-              <image
+            >节点量 {{ item1.totalVal }}
+            </text
+            >
+            <image
                 opacity="1"
                 stroke-width="1"
                 stroke-opacity="1"
                 fill-opacity="1"
-                :x="item3.trendImgX"
-                :y="item3.trendImgY"
+                :x="item1.trendImgX"
+                :y="item1.trendImgY"
                 width="18"
                 height="18"
                 :href="trendImg"
-              ></image>
-            </g>
-            <g>
-              <text
-                class="pro-text"
-                :x="item3.nodeX"
-                :y="item3.nodeY"
-                fill="#fff"
-                >节点量{{ item3.value }}</text
-              >
-              <image
-                opacity="1"
-                stroke-width="1"
-                stroke-opacity="1"
-                fill-opacity="1"
-                width="16"
-                height="16"
-                :x="item3.imgX"
-                :y="item3.imgY"
-                :href="InterfaceImg"
-              ></image>
-              <text
-                class="pro-text"
-                :x="item3.nameX"
-                :y="item3.nameY"
-                fill="#fff"
-                >{{ item3.abnormalVal }}</text
-              >
-              <image
-                opacity="1"
-                stroke-width="1"
-                stroke-opacity="1"
-                fill-opacity="1"
-                width="16"
-                height="16"
-                :x="item3.breakOffImgX"
-                :y="item3.breakOffImgY"
-                :href="breakOffImg"
-              ></image>
-            </g>
-            <image
+            ></image>
+          </g>
+          <image
               opacity="1"
               stroke-width="1"
               stroke-opacity="1"
               fill-opacity="1"
-              :x="item3.bottomX"
-              :y="item3.bottomY"
+              :x="item1.bottomX"
+              :y="item1.bottomY"
               width="15"
               height="15"
               :href="arrowUrl"
+          ></image>
+        </g>
+        <!-- 第二级 -->
+        <g
+            class="node"
+            width="200"
+            height="200"
+            v-for="(item2, index2) in item1.children"
+            :key="index2"
+        >
+          <!-- 联线 -->
+          <g>
+            <path :d="item2.pathD" stroke="#a09eff" fill="none"/>
+          </g>
+          <circle
+              :cx="item2.spreadX"
+              :cy="item2.spreadY"
+              r="6"
+              stroke="#fff"
+          ></circle>
+          <text
+              dy="4"
+              dx="-4"
+              :x="item2.spreadX"
+              :y="item2.spreadY"
+              fill="#fff"
+              font-size="13"
+              v-if="item2.isFold"
+          >+
+          </text
+          >
+          <text
+              dy="3"
+              dx="-2"
+              :x="item2.spreadX"
+              :y="item2.spreadY"
+              font-size="13"
+              fill="#fff"
+              v-else
+          >-
+          </text
+          >
+          <image
+              opacity="1"
+              stroke-width="1"
+              stroke-opacity="1"
+              fill-opacity="1"
+              width="100"
+              height="100"
+              :x="item2.imgX"
+              :y="item2.imgY"
+              :href="frontImg"
+          ></image>
+          <g>
+            <text
+                class="pro-text"
+                :x="item2.nameX"
+                :y="item2.nameY"
+                fill="#fff"
+            >{{ item2.name }}
+            </text
+            >
+            <image
+                opacity="1"
+                stroke-width="1"
+                stroke-opacity="1"
+                fill-opacity="1"
+                width="16"
+                height="16"
+                :x="item2.imgX"
+                :y="item2.imgY"
+                :href="InterfaceImg"
+            ></image>
+          </g>
+          <g>
+            <text
+                class="pro-num"
+                :x="item2.totalNodeX"
+                :y="item2.totalNodeY"
+                fill="#fff"
+                font="12"
+            >节点总量 {{ item2.totalVal }}
+            </text
+            >
+            <image
+                opacity="1"
+                stroke-width="1"
+                stroke-opacity="1"
+                fill-opacity="1"
+                :x="item2.trendImgX"
+                :y="item2.trendImgY"
+                width="18"
+                height="18"
+                :href="trendImg"
+            ></image>
+          </g>
+          <image
+              opacity="1"
+              stroke-width="1"
+              stroke-opacity="1"
+              fill-opacity="1"
+              :x="item2.bottomX"
+              :y="item2.bottomY"
+              width="15"
+              height="15"
+              :href="arrowUrl"
+          ></image>
+          <!-- 第三级 -->
+          <g
+              class="node"
+              width="200"
+              height="200"
+              v-for="(item3, index3) in item2.children"
+              :key="index3"
+          >
+            <!-- 联线 -->
+            <g>
+              <path :d="item3.pathD" stroke="red" fill="none"/>
+            </g>
+            <image
+                opacity="1"
+                stroke-width="1"
+                stroke-opacity="1"
+                fill-opacity="1"
+                width="100"
+                height="100"
+                :x="item3.imgX"
+                :y="item3.imgY"
+                :href="frontImg"
+            ></image>
+            <g>
+              <text
+                  class="pro-text"
+                  :x="item3.nameX"
+                  :y="item3.nameY"
+                  fill="#fff"
+              >{{ item3.name }}
+              </text
+              >
+              <image
+                  opacity="1"
+                  stroke-width="1"
+                  stroke-opacity="1"
+                  fill-opacity="1"
+                  width="16"
+                  height="16"
+                  :x="item3.InterfaceImgX"
+                  :y="item3.InterfaceImgY"
+                  :href="InterfaceImg"
+              ></image>
+            </g>
+            <g>
+              <text
+                  class="pro-num"
+                  :x="item3.totalNodeX"
+                  :y="item3.totalNodeY"
+                  fill="#fff"
+                  font="12"
+              >节点总量 {{ item3.totalVal }}
+              </text
+              >
+              <image
+                  opacity="1"
+                  stroke-width="1"
+                  stroke-opacity="1"
+                  fill-opacity="1"
+                  :x="item3.trendImgX"
+                  :y="item3.trendImgY"
+                  width="18"
+                  height="18"
+                  :href="trendImg"
+              ></image>
+            </g>
+            <g>
+              <text
+                  class="pro-text"
+                  :x="item3.nodeX"
+                  :y="item3.nodeY"
+                  fill="#fff"
+              >节点量{{ item3.value }}
+              </text
+              >
+              <image
+                  opacity="1"
+                  stroke-width="1"
+                  stroke-opacity="1"
+                  fill-opacity="1"
+                  width="16"
+                  height="16"
+                  :x="item3.imgX"
+                  :y="item3.imgY"
+                  :href="InterfaceImg"
+              ></image>
+              <text
+                  class="pro-text"
+                  :x="item3.nameX"
+                  :y="item3.nameY"
+                  fill="#fff"
+              >{{ item3.abnormalVal }}
+              </text
+              >
+              <image
+                  opacity="1"
+                  stroke-width="1"
+                  stroke-opacity="1"
+                  fill-opacity="1"
+                  width="16"
+                  height="16"
+                  :x="item3.breakOffImgX"
+                  :y="item3.breakOffImgY"
+                  :href="breakOffImg"
+              ></image>
+            </g>
+            <image
+                opacity="1"
+                stroke-width="1"
+                stroke-opacity="1"
+                fill-opacity="1"
+                :x="item3.bottomX"
+                :y="item3.bottomY"
+                width="15"
+                height="15"
+                :href="arrowUrl"
             ></image>
           </g>
           <!-- 第三级 -->
@@ -309,10 +292,11 @@
       </g>
     </g>
   </g>
-</template>    
+</template>
 
 <script>
 import d3Data from "./d3Data";
+
 export default {
   props: ["datasource"],
   data() {
@@ -330,7 +314,7 @@ export default {
   methods: {
     filter(targetId) {
       this.dataList = this.datasource.filer(
-        (item) => item.targetId == targetId
+          (item) => item.targetId == targetId
       );
     },
     show(i) {
@@ -347,9 +331,11 @@ export default {
 .progress {
   width: 800px;
 }
+
 .pro-text {
   font-size: 14px;
 }
+
 .pro-num {
   font-size: 14px;
 }
